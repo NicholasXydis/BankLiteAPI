22bb9b36842db305991034b0338d17a6e89f5fba
NicholasBuilds <nxydis879@gmail.com>
2026-03-29 15:32:57 -0400

    feat: add TransactionRepository

---
+++ BankLite.Infrastructure/Repositories/TransactionRepository.cs
@@ -0,0 +1,34 @@
+﻿using BankLite.Domain.Entities;
+using BankLite.Domain.Interfaces;
+using BankLite.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankLite.Infrastructure.Repositories
+{
+    public class TransactionRepository : ITransactionRepository
+    {
+        private readonly BankLiteDbContext _context;
+
+        public TransactionRepository(BankLiteDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Transaction>> GetByAccountIdAsync(Guid accountId)
+        {
+            return await _context.Transactions.Where(t => t.AccountId == accountId).ToListAsync();
+        }
+
+        public async Task AddAsync(Transaction transaction)
+        {
+            await _context.Transactions.AddAsync(transaction);
+            // TODO: Remove SaveChangesAsync from repo - will be handeled by service layer for atomic transfers
+            await _context.SaveChangesAsync();
+        }
+    }
+}
