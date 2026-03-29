d599bba024d4700d257fc873727770c359d560a0
NicholasBuilds <nxydis879@gmail.com>
2026-03-29 17:26:20 -0400

    feat: add Infrastructure project reference and configure DI in Program.cs

--- BankLiteAPI/BankLite.API/Program.cs
+++ BankLiteAPI/BankLite.API/Program.cs
@@ -1,3 +1,8 @@
+using BankLite.Infrastructure.Data;
+using BankLite.Infrastructure.Repositories;
+using BankLite.Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<BankLiteDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IAccountRepository, AccountRepository>();
+builder.Services.AddScoped<ITransactionRepository, TransactionRepository>();
+builder.Services.AddScoped<AuditLogRepository, AuditLogRepository>();
 
 var app = builder.Build();
 
