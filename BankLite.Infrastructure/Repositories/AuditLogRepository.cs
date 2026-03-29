0ad0704b54c8fce22408949a2374457aeaaf3538
NicholasBuilds <nxydis879@gmail.com>
2026-03-29 15:48:20 -0400

    feat: add AuditLogRepository

---
+++ BankLite.Infrastructure/Repositories/AuditLogRepository.cs
@@ -0,0 +1,33 @@
+﻿using BankLite.Domain.Entities;
+using BankLite.Domain.Interfaces;
+using BankLite.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankLite.Infrastructure.Repositories
+{
+    public class AuditLogRepository : IAuditLogRepository
+    {
+        private readonly BankLiteDbContext _context;
+
+        public AuditLogRepository(BankLiteDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<AuditLog>> GetByUserIdAsync(Guid userId)
+        {
+            return await _context.AuditLogs.Where(a => a.UserId == userId).ToListAsync();
+        }
+
+        public async Task LogAsync(AuditLog auditlog)
+        {
+            await _context.AuditLogs.AddAsync(auditlog);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
