84e6b5cff9ecde97f2eb7348c78298b7267f8d28
NicholasBuilds <nxydis879@gmail.com>
2026-03-29 11:09:09 -0400

    feat: add BankLiteDbContext

---
+++ BankLite.Infrastructure/Data/BankLiteDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BankLite.Domain.Entities;
+
+namespace BankLite.Infrastructure.Data
+{
+    public class BankLiteDbContext : DbContext
+    {
+        public BankLiteDbContext(DbContextOptions<BankLiteDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+        public DbSet<AuditLog> AuditLogs { get; set; }
+    }
+}
