65392e0a7f5762e385b4485c1b497e574b167b04
NicholasXydis <267418142+NicholasXydis@users.noreply.github.com>
2026-04-05 17:28:06 -0400

    feat: add AuthController with register and login endpoints

---
+++ BankLiteAPI/BankLite.API/Controllers/AuthController.cs
@@ -0,0 +1,33 @@
+﻿using BankLite.Application.DTOs;
+using BankLite.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BankLite.API.Controllers
+{
+        [ApiController]
+        [Route("api/[controller]")]
+
+        public class AuthController : ControllerBase
+        {
+            private readonly IAuthService _authService;
+
+            public AuthController(IAuthService authService)
+        {
+            _authService = authService; 
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterUserDto dto)
+        {
+            var result = await _authService.RegisterAsync(dto);
+            return Ok(result);
+        }
+
+        [HttpPost("login")]
+            public async Task<IActionResult> Login([FromBody] LoginUserDto dto)
+        {
+            var result = await _authService.LoginAsync(dto);
+            return Ok(result);
+        }
+    }
+}
