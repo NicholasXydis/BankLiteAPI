53c63afc85bb59e9a919b8f2ebdbc4f574869a54
NicholasXydis <267418142+NicholasXydis@users.noreply.github.com>
2026-04-06 22:20:37 -0400

    feat: add AccountController with JWT claim-based user identity

---
+++ BankLiteAPI/BankLite.API/Controllers/AccountController.cs
@@ -0,0 +1,38 @@
+﻿using BankLite.Application.DTOs;
+using BankLite.Application.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace BankLite.API.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AccountController : ControllerBase
+    {
+        private readonly IAccountService _accountService;   
+
+        public AccountController(IAccountService accountService)
+        {
+            _accountService = accountService;
+        }
+
+        [HttpPost("create")]
+        public async Task <IActionResult> CreateAccount([FromBody] CreateAccountDto dto)
+        {
+            var userId = Guid.Parse(User.FindFirst(ClaimTypes.NameIdentifier)!.Value);
+            var result = await _accountService.CreateAccountAsync(dto, userId);
+            return Ok(result);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAccounts()
+        {
+            var userId = Guid.Parse(User.FindFirst(ClaimTypes.NameIdentifier)!.Value);
+            var result = await _accountService.GetAccountsByUserIdAsync(userId);
+            return Ok(result);
+        }
+        
+    }
+}
