17981c3c3f84e8da6acc0bb726a914863ed349ef
NicholasXydis <267418142+NicholasXydis@users.noreply.github.com>
2026-04-11 13:27:49 -0400

    feat: prevent self-transfer in TransferValidator

--- BankLite.Application/Validators/TransferValidator.cs
+++ BankLite.Application/Validators/TransferValidator.cs
@@ -9,6 +9,7 @@ namespace BankLite.Application.Validators
         {
             RuleFor(x => x.Amount).GreaterThan(0);
             RuleFor(x => x.FromAccountId).NotEmpty();
+            RuleFor(x => x.ToAccountId).NotEqual(x => x.FromAccountId).WithMessage("Cannot transfer to the same account.");
             RuleFor(x => x.ToAccountId).NotEmpty();
         }
     }
