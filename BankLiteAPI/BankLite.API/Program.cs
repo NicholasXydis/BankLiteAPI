32028150437f8f818da0221e126a34f0be82687f
NicholasXydis <267418142+NicholasXydis@users.noreply.github.com>
2026-04-18 19:06:59 -0400

    fix: add SQL retry logic for transient connection errors

--- BankLiteAPI/BankLite.API/Program.cs
+++ BankLiteAPI/BankLite.API/Program.cs
@@ -54,7 +54,14 @@ builder.Services.AddSwaggerGen(c =>
 });
 
 builder.Services.AddDbContext<BankLiteDbContext>(options =>
-options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+options.UseSqlServer(
+    builder.Configuration.GetConnectionString("DefaultConnection"),
+    sqlOptions => sqlOptions.EnableRetryOnFailure(
+        maxRetryCount: 5,
+        maxRetryDelay: TimeSpan.FromSeconds(30),
+        errorNumbersToAdd: null
+        )
+    ));
 
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<IAccountRepository, AccountRepository>();
